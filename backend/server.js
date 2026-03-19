bb2ce425376342a5cf8f5b5f501e52c5282a2301
Matias Roldan <Matiasroldan695@gmail.com>
2026-03-19 00:06:33 -0300

    actualizacion puerto server.js

--- backend/server.js
+++ backend/server.js
@@ -90,5 +90,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Servidor corriendo en puerto ${PORT}`));
